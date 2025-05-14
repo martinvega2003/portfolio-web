36e554d7b923611d5d7c0b42445d27c2e2016362
martinvega2003 <josemartinvt2003@gmail.com>
2025-05-14 17:58:30 -0300

    Clean App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="text-center text-7xl text-red-500">
