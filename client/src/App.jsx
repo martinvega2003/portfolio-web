86273c3826e47fbd42e3513ab3d195d823443861
martinvega2003 <josemartinvt2003@gmail.com>
2025-05-14 18:12:52 -0300

    Create LanguageProvider.jsx file and wrap App.jsx content in it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
-import React, {useContext} from "react"
+import React from "react"
 import { ThemeModeProvider } from "./context/ThemeModeContext.jsx"
+import { LanguageProvider } from "./context/LanguageContext.jsx"
 import Button from "./components/Button.jsx"
 
 function App() {
 
   return (
-    <ThemeModeProvider>
-      <div className='text-5xl text-amber-300 dark:text-amber-700 dark:bg-gray-800'>App</div>
-      <Button />
-    </ThemeModeProvider>
+    <LanguageProvider>
+      <ThemeModeProvider>
+        <div className='text-5xl text-amber-300 dark:text-amber-700 dark:bg-gray-800'>App</div>
+        <Button />
+      </ThemeModeProvider>
+    </LanguageProvider>
   )
 }
 
