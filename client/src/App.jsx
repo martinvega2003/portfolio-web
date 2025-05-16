1cb8dcc9fc3ab3e427066a5dcb08e082bcb49abf
martinvega2003 <josemartinvt2003@gmail.com>
2025-05-16 12:32:11 -0300

    Add fixed image background in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { ThemeModeProvider } from "./context/ThemeModeContext"
 import { LanguageProvider } from "./context/LanguageContext"
 import Mainpage from "./pages/Mainpage"
 import Navbar from "./components/Navbar"
+import bgImageUrl from './images/working-bg.webp'
 
 function App() {
 
@@ -11,6 +12,15 @@ function App() {
       <LanguageProvider>
         <ThemeModeProvider>
           <BrowserRouter>
+            {/* Background Image Layer */}
+            <div
+              className="fixed inset-0 bg-fixed bg-cover bg-center bg-no-repeat opacity-50"
+              style={{
+                backgroundImage: `url(${bgImageUrl})`,
+                zIndex: -1,
+                //backgroundAttachment: "scroll", // Ensures smooth scrolling
+              }}
+            ></div>
             <Navbar />
             <Routes>
               <Route path="/" element={<Mainpage />} />
