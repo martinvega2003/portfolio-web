b4c655d5f8cd9d53a45e6cdd765347cabfb67a90
martinvega2003 <josemartinvt2003@gmail.com>
2025-05-16 10:00:18 -0300

    Create a Mainpage component

---
+++ client/src/pages/Mainpage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from './section/HeroSection'
+import Button from '../components/Button'
+
+const Mainpage = () => {
+  return (
+    <main>
+      <HeroSection />
+      <Button />
+    </main>
+  )
+}
+
+export default Mainpage
