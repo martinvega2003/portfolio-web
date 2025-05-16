cb4bb75b0093f5f201ede7a2df6d763fdddfb276
martinvega2003 <josemartinvt2003@gmail.com>
2025-05-16 12:35:50 -0300

    Create a job fields section and add it in the Main page component

--- client/src/pages/Mainpage.jsx
+++ client/src/pages/Mainpage.jsx
@@ -3,6 +3,7 @@ import HeroSection from './section/HeroSection'
 import AboutMeSection from './section/AboutMeSection'
 import SoftSkillsSection from './section/SoftSkillsSection'
 import CVSection from './section/CVSection'
+import FieldsSection from './section/FieldsSection'
 
 const Mainpage = () => {
   return (
@@ -11,6 +12,7 @@ const Mainpage = () => {
       <AboutMeSection />
       <SoftSkillsSection />
       <CVSection />
+      <FieldsSection />
     </main>
   )
 }
