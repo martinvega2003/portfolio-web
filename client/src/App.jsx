26a2c2f023d7c683df368f908d5385ff40c10ee4
martinvega2003 <josemartinvt2003@gmail.com>
2025-05-16 10:00:53 -0300

    Add Language context and react-router-dom components in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
 import React from "react"
-import { ThemeModeProvider } from "./context/ThemeModeContext.jsx"
-import { LanguageProvider } from "./context/LanguageContext.jsx"
-import Button from "./components/Button.jsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { ThemeModeProvider } from "./context/ThemeModeContext"
+import { LanguageProvider } from "./context/LanguageContext"
+import Mainpage from "./pages/Mainpage"
 
 function App() {
 
   return (
-    <LanguageProvider>
-      <ThemeModeProvider>
-        <div className='text-5xl text-amber-300 dark:text-amber-700 dark:bg-gray-800'>App</div>
-        <Button />
-      </ThemeModeProvider>
-    </LanguageProvider>
+      <LanguageProvider>
+        <ThemeModeProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Mainpage />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeModeProvider>
+      </LanguageProvider>
   )
 }
 
