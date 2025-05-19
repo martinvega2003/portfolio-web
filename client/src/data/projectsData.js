2e95d861f3ea2f06f02ba02b35f1a84d0781512f
martinvega2003 <josemartinvt2003@gmail.com>
2025-05-19 12:11:30 -0300

    Update projectsData file with real projects from myself and their thumbnail

--- client/src/data/projectsData.js
+++ client/src/data/projectsData.js
@@ -1,26 +1,26 @@
-import imgOne from "../images/project-one-thumbnail.png"
-import imgTwo from "../images/project-two-thumbnail.png"
-import imgThree from "../images/project-three-thumbnail.png"
-import imgFour from "../images/project-four-thumbnail.png"
-import { SiReact, SiTailwindcss, SiExpress, SiPostgresql } from "react-icons/si";
-import { MdChat, MdSearch, MdBrush, MdCode } from "react-icons/md";
+import imgOne from "../images/project-manager-thumbnail.png"
+import imgTwo from "../images/software-website-thumbnail.png"
+import imgThree from "../images/redux-to-do-app-thumbnail.png"
+import { SiReact, SiTailwindcss, SiExpress, SiPostgresql, SiRedux } from "react-icons/si";
+import { FaLaptopCode, FaTasks } from "react-icons/fa";
+import { BsCalendarCheck } from "react-icons/bs";
 
 export const projectsData = [
   {
     id: 1,
     number: "I",
-    icon: MdChat,
+    icon: BsCalendarCheck,
     title: {
-      en: "Project I",
-      es: "Proyecto I",
+      en: "Project Manager",
+      es: "Gestor de Proyectos",
     },
     image: imgOne || "https://via.placeholder.com/300",
     description: {
-      en: "This project showcases a seamless user experience designed for effective business solutions. It leverages modern technologies to address real-world challenges efficiently.",
-      es: "Este proyecto muestra una experiencia de usuario fluida diseñada para soluciones empresariales efectivas. Aprovecha tecnologías modernas para abordar desafíos del mundo real de manera eficiente.",
+      en: 'A robust project management platform enabling users to create accounts, manage teams, and oversee projects with ease. Features include team member sub-accounts, project and task creation, calendar scheduling, and seamless assignment of responsibilities for efficient collaboration.',
+      es: 'Una plataforma de gestión de proyectos robusta que permite a los usuarios crear cuentas, gestionar equipos y supervisar proyectos fácilmente. Incluye subcuentas para miembros del equipo, creación de proyectos y tareas, programación en calendario y asignación eficiente de responsabilidades para una colaboración fluida.',
     },
-    linkToCode: "https://github.com/martinvega2003/dentist-site-project-one",
-    linkToDeployment: "https://dentist-site-project-one.vercel.app/",
+    linkToCode: "https://github.com/martinvega2003/devops-task-manager",
+    linkToDeployment: "https://project-manager-frontend-peach.vercel.app/login",
     technologies: [
       { name: "React", color: "text-cyan-300", icon: SiReact },
       { name: "TailwindCSS", color: "text-blue-500", icon: SiTailwindcss },
@@ -31,18 +31,18 @@ export const projectsData = [
   {
     id: 2,
     number: "II",
-    icon: MdSearch,
+    icon: FaLaptopCode,
     title: {
-      en: "Project II",
-      es: "Proyecto II",
+      en: "Software Website",
+      es: "Pagina Web de Software",
     },
     image: imgTwo || "https://via.placeholder.com/300",
     description: {
-      en: "A comprehensive project aimed at enhancing online visibility and driving engagement through innovative features tailored to clients' needs.",
-      es: "Un proyecto integral destinado a mejorar la visibilidad en línea y aumentar el compromiso a través de funciones innovadoras adaptadas a las necesidades de los clientes.",
+      en: "A modern website offering custom software solutions tailored for healthcare businesses. Showcases services, expertise, and a commitment to delivering innovative digital products that streamline operations and enhance client satisfaction.",
+      es: "Un sitio web moderno que ofrece soluciones de software personalizadas para empresas del sector salud. Presenta servicios, experiencia y un compromiso con la entrega de productos digitales innovadores que optimizan operaciones y mejoran la satisfacción del cliente.",
     },
-    linkToCode: "https://github.com/martinvega2003/dentist-site-project-two",
-    linkToDeployment: "https://dentist-site-project-two.vercel.app/",
+    linkToCode: "https://github.com/martinvega2003/custom-healthcare-software-agency-website",
+    linkToDeployment: "https://custom-software-website-frontend.vercel.app",
     technologies: [
       { name: "React", color: "text-cyan-300", icon: SiReact },
       { name: "TailwindCSS", color: "text-blue-500", icon: SiTailwindcss },
@@ -53,45 +53,22 @@ export const projectsData = [
   {
     id: 3,
     number: "III",
-    icon: MdBrush,
+    icon: FaTasks,
     title: {
-      en: "Project III",
-      es: "Proyecto III",
+      en: "To-do App w/Redux",
+      es: "Lista de tareas con Redux",
     },
     image: imgThree || "https://via.placeholder.com/300",
     description: {
-      en: "An advanced solution for optimizing workflows and improving performance metrics, ensuring scalability and efficiency.",
-      es: "Una solución avanzada para optimizar flujos de trabajo y mejorar métricas de rendimiento, garantizando escalabilidad y eficiencia.",
+      en: "A simple yet effective to-do application built with Redux for state management. Allows users to add, complete, and organize tasks, providing a practical environment to boost productivity and practice modern React development techniques.",
+      es: "Una aplicación de tareas sencilla pero efectiva, desarrollada con Redux para la gestión de estado. Permite a los usuarios agregar, completar y organizar tareas, ofreciendo un entorno práctico para aumentar la productividad y practicar técnicas modernas de desarrollo con React.",
     },
-    linkToCode: "https://github.com/martinvega2003/dentist-site-project-three",
-    linkToDeployment: "https://dentist-site-project-three.vercel.app/",
+    linkToCode: "https://github.com/martinvega2003/redux-toolkit",
+    linkToDeployment: "https://redux-toolkit-to-do-app-five.vercel.app",
     technologies: [
       { name: "React", color: "text-cyan-300", icon: SiReact },
       { name: "TailwindCSS", color: "text-blue-500", icon: SiTailwindcss },
-      { name: "Express.js", color: "text-yellow-300", icon: SiExpress },
-      { name: "PostgreSQL", color: "text-green-400", icon: SiPostgresql },
-    ],
-  },
-  {
-    id: 4,
-    number: "IV",
-    icon: MdCode,
-    title: {
-      en: "Project IV",
-      es: "Proyecto IV",
-    },
-    image: imgFour || "https://via.placeholder.com/300",
-    description: {
-      en: "An advanced solution for optimizing workflows and improving performance metrics, ensuring scalability and efficiency.",
-      es: "Una solución avanzada para optimizar flujos de trabajo y mejorar métricas de rendimiento, garantizando escalabilidad y eficiencia.",
-    },
-    linkToCode: "https://github.com/martinvega2003/dentist-site-project-four",
-    linkToDeployment: "https://dentist-site-project-four.vercel.app/",
-    technologies: [
-      { name: "React", color: "text-cyan-300", icon: SiReact },
-      { name: "TailwindCSS", color: "text-blue-500", icon: SiTailwindcss },
-      { name: "Express.js", color: "text-yellow-300", icon: SiExpress },
-      { name: "PostgreSQL", color: "text-green-400", icon: SiPostgresql },
+      { name: "Redux-toolkit", color: "text-purple-500", icon: SiRedux },
     ],
   },
 ];
