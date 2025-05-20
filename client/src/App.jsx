bcad81ed01e633b4662b9c6ad187010ab644a915
martinvega2003 <josemartinvt2003@gmail.com>
2025-05-20 11:51:15 -0300

    Move BrowserRouter and Context Providers one layer above (main.jsx)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { ThemeModeProvider } from "./context/ThemeModeContext"
-import { LanguageProvider } from "./context/LanguageContext"
+import { Routes, Route } from "react-router-dom"
 import Mainpage from "./pages/Mainpage"
 import Form from "./pages/Form"
 import Navbar from "./components/Navbar"
@@ -14,28 +12,24 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
 
   return (
-      <LanguageProvider>
-        <ThemeModeProvider>
-          <BrowserRouter>
-            {/* Background Image Layer */}
-            <div
-              className="fixed inset-0 bg-fixed bg-cover bg-center bg-no-repeat opacity-50"
-              style={{
-                backgroundImage: `url(${bgImageUrl})`,
-                zIndex: -1,
-              }}
-            ></div>
-            <ToastContainer position="top-right" autoClose={3000} />
-            <ScrollToTop />
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Mainpage />} />
-              <Route path="/form" element={<Form />} />
-            </Routes>
-            <Footer />
-          </BrowserRouter>
-        </ThemeModeProvider>
-      </LanguageProvider>
+    <>
+      {/* Background Image Layer */}
+      <div
+        className="fixed inset-0 bg-fixed bg-cover bg-center bg-no-repeat opacity-50"
+        style={{
+          backgroundImage: `url(${bgImageUrl})`,
+          zIndex: -1,
+        }}
+      ></div>
+      <ToastContainer position="top-right" autoClose={3000} />
+      <ScrollToTop />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Mainpage />} />
+        <Route path="/form" element={<Form />} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
